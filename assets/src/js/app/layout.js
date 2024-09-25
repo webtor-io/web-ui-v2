c7ba1a82efb1026ce3447c6537e46052a713b3b7
Pavel Tatarskiy <pavel.tatarsky@gmail.com>
2024-09-25 22:06:23 +0300

    revert fix of theme selector

--- assets/src/js/app/layout.js
+++ assets/src/js/app/layout.js
@@ -13,13 +13,14 @@ window.progress = {
 };
 
 import {bindAsync} from '../lib/async';
-const av = (await import('../lib/asyncView')).initAsyncView;
+// const av = (await import('../lib/asyncView')).initAsyncView;
 import themeSelector from "../lib/themeSelector";
 
 function onLoad() {
-    av(document.querySelector('nav'), 'index', async function() {
-        themeSelector(document.querySelector('[data-toggle-theme]'));
-    });
+    themeSelector(document.querySelector('[data-toggle-theme]'));
+    // av(document.querySelector('nav'), 'index', async function() {
+    //     themeSelector(document.querySelector('[data-toggle-theme]'));
+    // });
     document.body.style.display = 'flex';
     hideProgress();
     bindAsync({
