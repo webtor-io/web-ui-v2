488e85515f632fa5ad8b4534aaa290f2d77f78c8
Pavel Tatarskiy <pavel.tatarsky@gmail.com>
2024-11-18 10:27:48 +0300

    add examples listing

--- handlers/embed/example/handler.go
+++ handlers/embed/example/handler.go
@@ -1,22 +1,43 @@
 package example
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 	"github.com/webtor-io/web-ui-v2/services/template"
+	"github.com/yargevad/filepathx"
+	"net/http"
+	"path/filepath"
+	"strings"
 )
 
 type Handler struct {
-	tb template.Builder
+	tb       template.Builder
+	examples Examples
 }
 
+type Example struct {
+	Name string
+}
+
+type Examples []Example
+
 func RegisterHandler(r *gin.Engine, tm *template.Manager) {
-	h := &Handler{
-		tb: tm.MustRegisterViews("embed/example/*").WithLayout("embed/example"),
+	examples := Examples{}
+	g, err := filepathx.Glob("templates/views/embed/example/*")
+	if err != nil {
+		panic(err)
+	}
+	for _, f := range g {
+		examples = append(examples, Example{
+			Name: strings.TrimSuffix(filepath.Base(f), ".html"),
+		})
 	}
 
+	h := &Handler{
+		tb:       tm.MustRegisterViews("embed/example/*").WithLayout("embed/example"),
+		examples: examples,
+	}
 	r.GET("/embed/example/:name", h.get)
+	r.GET("/embed/example", h.exampleIndex)
 }
 
 type Data struct {
@@ -24,4 +45,8 @@ type Data struct {
 
 func (s *Handler) get(c *gin.Context) {
 	s.tb.Build("embed/example/"+c.Param("name")).HTML(http.StatusOK, c, &Data{})
+}
+
+func (s *Handler) exampleIndex(c *gin.Context) {
+	s.tb.Build("embed/example/index").HTML(http.StatusOK, c, s.examples)
 }
